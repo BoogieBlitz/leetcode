34efc6913431ca483f1e23da81a89ac5e6fd0f9a
boogieblitz <dlaststark@gmail.com>
2024-05-27 00:01:45 +0530

    add_two_numbers optimized solution

--- add_two_numbers/src/main.rs
+++ add_two_numbers/src/main.rs
@@ -4,19 +4,31 @@ pub struct ListNode {
   pub next: Option<Box<ListNode>>
 }
 
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
 /// Add Two Numbers
 /// https://leetcode.com/problems/add-two-numbers/description/
 pub fn add_two_numbers(mut l1: Option<Box<ListNode>>, mut l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     // Lists to store carry values and digits for final sum
-    let mut carry_vec: Vec<i32> = Vec::new();
-    let mut result_vec: Vec<i32> = Vec::new();
+    let mut carry: i32 = 0;
+    let mut result: ListNode = ListNode::new(0);
+    let mut current: &mut ListNode = &mut result;
 
     loop {
-        // Get the carry value
-        let carry: i32 = match carry_vec.pop() {
-            Some(s) => s,
-            None => 0
-        };
+        if (carry == 0) & (l1 == None) & (l2 == None)  {
+            break;
+        }
+
+        // Initializing sum value
+        let mut sum: i32 = carry;
 
         // Get the value from left number
         let num1: i32 = match l1.as_ref() {
@@ -30,59 +42,23 @@ pub fn add_two_numbers(mut l1: Option<Box<ListNode>>, mut l2: Option<Box<ListNod
             None => 0
         };
 
-        // Sum all numbers + carry value
-        let mut sum: i32 = num1 + num2 + carry;
-        
-        // Incase sum value > 9, then calculate carry and resulting sum digit
-        if sum > 9 {
-            carry_vec.push(1);
-            sum = sum - 10;
-        }
+        // Sum all numbers, and calculate carry value
+        sum += num1 + num2;
+        carry = sum / 10;
 
         // Keep track of result digits
-        result_vec.push(sum);
+        current.next = Some(Box::new(ListNode::new(sum % 10)));
+        current = current.next.as_mut().unwrap();
 
-        if (carry_vec.len() == 0) & (l1 == None) & (l2 == None)  {
-            break;
-        } else {
-            if l1 != None {
-                l1 = l1.clone().unwrap().next;
-            }
-            
-            if l2 != None {
-                l2 = l2.clone().unwrap().next;
-            }
+        if l1 != None {
+            l1 = l1.clone().unwrap().next;
+        }
+        
+        if l2 != None {
+            l2 = l2.clone().unwrap().next;
         }
     }
-    if result_vec.get(result_vec.len()-1).unwrap() == &0 {
-        result_vec.pop();
-    }
-    create_linked_list(result_vec)
-}
-
-// Convert digits in result vector to Linked List
-pub fn create_linked_list(result_vec: Vec<i32>) -> Option<Box<ListNode>> {
-    let mut temp: Option<Box<ListNode>> = None;
-    let mut cache: Option<Box<ListNode>> = None;
-    for (idx, num) in result_vec.iter().rev().enumerate() {
-        if idx == 0 {
-            temp = Some(Box::new(
-                ListNode {
-                    val: *num,
-                    next: None
-                }
-            ));
-        } else {
-            temp = Some(Box::new(
-                ListNode {
-                    val: *num,
-                    next: cache
-                }
-            ))
-        }
-        cache = temp.clone();
-    }
-    temp
+    result.next
 }
 
 fn main() {
